785e75c34da2ca6d72bfb40559863491eb78b7ef
Balram Rathore <balram0698@gmail.com>
2020-10-17 17:42:38 +0530

    new navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import Heading from './Heading'
-import Home from './Home'
-import Search from './Search'
-import {BrowserRouter , Route} from 'react-router-dom'
-const App=()=>{
-    return(<BrowserRouter>
-      <Heading/>
+import React from "react";
+// import Heading from "./Heading";
+import Home from "./Home";
+import Search from "./Search";
+import NavBar from "./NavBar";
+import { BrowserRouter, Route } from "react-router-dom";
+const App = () => {
+  return (
+    <BrowserRouter>
+      {/* <Heading/> */}
+      <NavBar />
       <Route path="/" exact component={Home}></Route>
       <Route path="/:name" component={Search}></Route>
-      </BrowserRouter>
-    )
-}
+    </BrowserRouter>
+  );
+};
 
 export default App;
