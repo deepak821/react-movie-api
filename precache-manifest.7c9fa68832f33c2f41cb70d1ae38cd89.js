2be47122850832725b161750f0ab2f794e5a1212
deepak821 <pandeydeepak821@gmail.com>
2020-12-22 14:04:17 +0530

    Updates

---
+++ precache-manifest.7c9fa68832f33c2f41cb70d1ae38cd89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29a0fc7ce8cc8c381a1d143cf328a799",
+    "url": "/react-movie-api/index.html"
+  },
+  {
+    "revision": "6284f2e8f2a4d51db8da",
+    "url": "/react-movie-api/static/css/main.e36ca292.chunk.css"
+  },
+  {
+    "revision": "4cf01dab3cad6671f192",
+    "url": "/react-movie-api/static/js/2.97ee984c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-movie-api/static/js/2.97ee984c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6284f2e8f2a4d51db8da",
+    "url": "/react-movie-api/static/js/main.ef3bb3ca.chunk.js"
+  },
+  {
+    "revision": "5cb2213f969115fd8ea2",
+    "url": "/react-movie-api/static/js/runtime-main.fb2905bd.js"
+  }
+]);
